95f20d16af8ef7f11e1e959a521da0eada03407b
RodrigoC3805 <rodrigocastillo3805@hotmail.com>
2025-05-07 20:55:13 -0500

    added navbar and home element

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ProductosComponent } from './component/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'productos', component: ProductosComponent}
+];
---
+++ src/app/component/realizar-compra/realizar-compra.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realizar-compra',
+  imports: [],
+  templateUrl: './realizar-compra.component.html',
+  styleUrl: './realizar-compra.component.css'
+})
+export class RealizarCompraComponent {
+
+}
